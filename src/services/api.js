83f4eb1fb527d9900feb2788b42002d32993f4f6
Lakshmankashyap007 <lakshmankashyap.yt@gmail.com>
2026-07-22 15:18:44 +0530

    Configure API URL for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+import axios from "axios";
 const API = axios.create({
-    baseURL: "http://localhost:3004/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
+
+export default API;
 
 // export const getDonors = (bloodGroup = "", city = "") =>
 //     API.get(
